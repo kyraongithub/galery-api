90e254d8deb7d30dfac918525ca9d00fe44ea0bd
Rizky Ramadhan <71491992+kyraongithub@users.noreply.github.com>
2023-03-31 14:46:03 +0700

    add search by tag feature

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -16,3 +16,23 @@ export const getUserImages = async (req: Request, res: Response) => {
     return res.status(200).json({ status: true, statusCode: 200, message: 'data fetched successfully', data: result })
   })
 }
+
+export const getImagesByTag = async (req: Request, res: Response) => {
+  const xmlData = await axios.get('https://api.flickr.com/services/feeds/photos_public.gne', {
+    headers: {
+      'Content-Type': 'application/xml'
+    },
+    params: {
+      tags: req.query.tag
+    }
+  })
+
+  console.log(req)
+
+  xml2js.parseString(xmlData.data, (err: any, result: any) => {
+    if (err) {
+      return res.status(500).json({ status: false, statusCode: 500, message: 'failed to fetch data' })
+    }
+    return res.status(200).json({ status: true, statusCode: 200, message: 'data fetched successfully', data: result })
+  })
+}
--- src/routes/images.route.ts
+++ src/routes/images.route.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { apiLimiter } from '../middleware/rateLimit'
-import { getUserImages } from '../controllers/images.controller'
+import { getUserImages, getImagesByTag } from '../controllers/images.controller'
 
 export const imagesRoute: Router = Router()
 
 imagesRoute.get('/', apiLimiter, getUserImages)
+imagesRoute.get('/tags', apiLimiter, getImagesByTag)
